096ca185c9b6be5498eddbc6a5f1079e9a696eea
genebit <genebitara@gmail.com>
2021-06-18 13:36:49 +0800

    Update README, update to sketch.js

---
+++ README.md
@@ -0,0 +1,4 @@
+## About
+Audio Visualizer built in p5js    
+### Sources
+* _https://nishanc.medium.com/audio-visualization-in-javascript-with-p5-js-cf3bc7f1be07_
--- sketch.js
+++ sketch.js
@@ -1,7 +1,19 @@
+
+let microphone;
+
 function setup() {
   createCanvas(500, 500);
+
+  microphone = new p5.AudioIn();
+  microphone.start()
 }
 
 function draw() {
   background('255');
+
+  fill(26)
+  noStroke()
+  let volume = microphone.getLevel();
+  ellipse(height/2, width/2, volume*500, volume*500);
+  console.log('volume', volume);
 }
