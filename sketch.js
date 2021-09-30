0af88d3e007b7cfb38fef099acbf36cdc624eab7
genebit <genebitara@gmail.com>
2021-09-30 21:51:30 +0800

    Hooking the dom elements in p5js controls

--- sketch.js
+++ sketch.js
@@ -1,23 +1,20 @@
 
 var canvas;
 var fft;
-const bands = 128;
-const smoothing = 0.7;
+// 2 4 8 16 32 64 128 256 512
+var bands = 128;
+var smoothing = 0.8;
 
 var microphone;
-
-let song;
 
 function setup() {
     canvas = createCanvas(windowWidth, 400);
     canvas.parent("#sketch");
-    
+    frameRate(60);
+
     microphone = new p5.AudioIn();
     microphone.start();
     getAudioContext().resume();
-
-    // Sound Setup
-    song = loadSound("./sound/kokeko🐔.mp3");
 
     // FFT Setup
     fft = new p5.FFT(smoothing, bands);
@@ -27,25 +24,22 @@ function setup() {
 var distance = 2; 
 
 function draw() {
-    background(26);
+    background($("#backg-color").val());
     
     let spectrum = fft.analyze();
     let position = height / 2;
     
-    fill("#Ed4E4E");
+    // "#Ed4E4E"
+    fill($("#spectrum-color").val());
     noStroke();
 
     for (let i = 0; i < spectrum.length; i++) {
         let x = map(i, 0, spectrum.length, 0, width);
         let bandHeight = -height + map(spectrum[i], 0, 255, height, 0);
-        // rect(x * 2, height-20, 5, bandHeight);
         ellipse(x * 1.5, position, 100, bandHeight);
+        // rect(x * 2, height-20, 5, bandHeight);
     }
 }
 
-function toggleSong() {
-    if (song.isPlaying())
-        song.stop();
-    else
-        song.play();
-}
+var toggleMic = function() { microphone.start(); }
+var pauseMic = function() { microphone.stop(); }
