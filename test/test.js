663d6c38a5c000508cc2ff71012d7dd545d957ff
genebit <genebitara@gmail.com>
2021-10-13 12:04:07 +0800

    Created gitignore

--- test/test.js
+++ test/test.js
@@ -1,15 +1,26 @@
 
-var sound;
+var canvas;
+var fft;
+var microphone;
+
+function setup() {
+    canvas = createCanvas(500, 500);
+    microphone = new p5.AudioIn();
+    microphone.start();
+
+    // fft = new p5.FFT(0.7, 16);
+    // fft.setInput(microphone);
+}
 
 function draw() {
-    document.getElementById("upload").addEventListener("change", handleFiles, false);
+    background(200);
+
+    let volume = microphone.getLevel();
+    // let spectrum = fft.analyze();
+    // console.log(spectrum)
+    
+    // map(value, (original values min and max), (new values min and max))
+
+    let size = map(volume, 0, 1, 0, canvas.height)
+    ellipse(canvas.width/2, canvas.height/2, size, size);
 }
-
-function handleFiles(event) {
-    var files = event.target.files;
-    $("#src").attr("src", URL.createObjectURL(files[0]));
-    document.getElementById("audio").load();
-
-    document.getElementById("src").createAtrribute("download");
-}
-
