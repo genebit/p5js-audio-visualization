f688007f031793a2feb926401011c302c430f2e0
genebit <genebitara@gmail.com>
2023-06-27 21:26:25 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,15 +1,17 @@
-var canvas;
-var fft;
-// 2 4 8 16 32 64 128 256 512
-var bands = 128;
-var smoothing = 0.8;
+let fft;
+let soundFile;
 
-var microphone;
+let bands = 256; // 2 4 8 16 32 64 128 256 512
+let smoothing = 0.8;
+let distance = 2;
+let microphone;
 
 function setup() {
-	canvas = createCanvas(windowWidth, 400);
-	canvas.parent("#sketch");
-	frameRate(60);
+	let $sketchContainer = $("#sketchContainer");
+	let cWidth = $sketchContainer.width();
+	let cHeight = $sketchContainer.height();
+	let canvas = createCanvas(cWidth, cHeight);
+	canvas.parent("sketchContainer");
 
 	microphone = new p5.AudioIn();
 	microphone.start();
@@ -18,8 +20,6 @@ function setup() {
 	fft = new p5.FFT(smoothing, bands);
 	fft.setInput(microphone);
 }
-
-var distance = 2;
 
 function draw() {
 	getAudioContext().resume();
@@ -33,10 +33,11 @@ function draw() {
 
 	for (let i = 0; i < spectrum.length; i++) {
 		let x = map(i, 0, spectrum.length, 0, width);
-		let bandHeight = -height + map(spectrum[i], 0, 255, 500, 0);
+		let bandHeight = map(spectrum[i], 0, 255, 0, height / 2);
 		selectStyle($("#spectrum-style").val(), x, position, bandHeight);
 	}
 }
+
 function selectStyle(selected, x, pos, bandheight) {
 	switch (selected) {
 		case "Intestine":
@@ -44,7 +45,7 @@ function selectStyle(selected, x, pos, bandheight) {
 			break;
 
 		case "Rectangle":
-			rect(x * distance, height - 20, 15, bandheight);
+			rect(x * distance, height - bandheight, 15, bandheight);
 			break;
 
 		case "Hypno":
@@ -53,16 +54,45 @@ function selectStyle(selected, x, pos, bandheight) {
 			strokeWeight(3);
 			ellipse(width / 2, height / 2, bandheight, bandheight);
 			break;
+
+		case "Mirror":
+			let rectWidth = 5;
+			let rectX = x * distance - rectWidth / 2;
+			let rectY = height / 2 - bandheight / 2;
+
+			// Draw top rectangle
+			rect(rectX, rectY, rectWidth, bandheight);
+
+			// Draw bottom rectangle
+			let bottomRectY = height / 2 + bandheight / 2;
+			rect(rectX, bottomRectY, rectWidth, -bandheight, 50);
+			break;
 	}
 }
 
-var toggleMic = function () {
+let toggleMic = function () {
 	microphone.start();
 };
-var pauseMic = function () {
+let pauseMic = function () {
 	microphone.stop();
 };
 
 function touchStarted() {
 	getAudioContext().resume();
 }
+
+$(document).ready(function () {
+	// Listen for change event on file input
+	$("#audioFile").on("change", function (e) {
+		var file = e.target.files[0];
+		var audio = $("#audioControls")[0];
+
+		// Update src attribute of audio element
+		var fileURL = URL.createObjectURL(file);
+		audio.src = fileURL;
+
+		let audioElement = $("#audioControls")[0];
+		soundFile = new p5.SoundFile(audioElement.src);
+		fft.setInput(soundFile);
+	});
+});
