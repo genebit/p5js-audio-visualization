a60843c31fed71a934191785fbb81c234044289e
genebit <genebitara@gmail.com>
2023-06-30 15:11:40 +0800

    Updated band height

--- public/js/sketch.js
+++ public/js/sketch.js
@@ -61,7 +61,7 @@ function draw() {
 
 	for (let i = 0; i < spectrum.length; i++) {
 		let x = map(i, 0, spectrum.length, 0, width);
-		let bandHeight = map(spectrum[i], 0, 255, 0, height / 2);
+		let bandHeight = map(spectrum[i], 0, 255, 0, height);
 		selectStyle(spectrumStyle, x, position, bandHeight);
 	}
 }
