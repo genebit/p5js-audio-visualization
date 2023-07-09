524fde47b75fc7194f9b6692035a3740a224ecda
genebit <genebitara@gmail.com>
2023-07-09 13:20:31 +0800

    Implemented remaining spectrum styles

--- public/js/sketch.js
+++ public/js/sketch.js
@@ -62,6 +62,65 @@ function draw() {
 					x += rectWidth + padding;
 				}
 				break;
+			case "Clouds":
+				fill(spectrumColor);
+				noStroke();
+
+				for (let i = 0; i < spectrum.length; i++) {
+					let x = map(i, 0, spectrum.length, 0, width);
+					let bandHeight = map(spectrum[i], 0, 255, 0, height);
+
+					ellipse(x * 2, height, 200, bandHeight);
+				}
+				break;
+			case "Ellipse":
+				fill(spectrumColor);
+				noStroke();
+				const bassAmplitude = fft.getEnergy("bass");
+				const ellipseSize = map(bassAmplitude, 0, 255, 10, 200);
+
+				ellipse(width / 2, height / 2, ellipseSize, ellipseSize);
+				break;
+			case "Wavelength":
+				let waveform = fft.waveform();
+				noFill();
+				beginShape();
+
+				stroke(spectrumColor);
+				strokeWeight(3);
+				for (let i = 0; i < waveform.length; i++) {
+					let x = map(i, 0, waveform.length, 0, width);
+					let y = map(waveform[i], -1, 1, 0, height);
+					vertex(x, y);
+				}
+				endShape();
+				break;
+			case "Hypnosis":
+				noFill();
+				stroke(spectrumColor);
+				strokeWeight(3);
+				for (let i = 0; i < spectrum.length; i++) {
+					let bandHeight = map(spectrum[i], 0, 255, 0, height);
+
+					ellipse(width / 2, height / 2, bandHeight, bandHeight);
+				}
+				break;
+			case "Mirror":
+				fill(spectrumColor);
+				noStroke();
+				for (let i = 0; i < spectrum.length; i++) {
+					let x = map(i, 0, spectrum.length, 0, width);
+					let bandHeight = map(spectrum[i], 0, 255, 0, height);
+
+					let rectW = 10;
+					let rectX = x * 2 - rectW / 2;
+					let rectY = height / 2 - bandHeight / 2;
+
+					rect(rectX, rectY, rectW, bandHeight, 100);
+					let bottomRectY = height / 2 + bandHeight / 2;
+					rect(rectX, bottomRectY, rectW, -bandHeight, 100);
+				}
+				break;
 		}
 	}
 }
